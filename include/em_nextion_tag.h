f58b74082a1d5157e75e2578119e47c9ee4400c8
cabbi <fabio@cabella.net>
2025-12-11 15:05:14 +0100

    WIP

--- include/em_nextion_tag.h
+++ include/em_nextion_tag.h
@@ -256,7 +256,7 @@ public:
     virtual bool updateValue(EmOptional<int32_t> minValue = emUndefined,
                              EmOptional<int32_t> maxValue = emUndefined,
                              EmOptional<int32_t> dispInitialValue = emUndefined) {
-        // DIDN'T get compiler accept this syntax.... :()
+        // DIDN'T get the compiler to accept this syntax :(
         //return EmNexTagBase<EmNexCfgInteger<tPage>, int32_t>::updateValue<EmTagBase>(*this, minValue, maxValue, dispInitialValue);
         return EmNexTagBase<EmNexCfgInteger<tPage>, int32_t>::updateValue(static_cast<EmTagBase&>(*this), minValue, maxValue, dispInitialValue);
     }                                
